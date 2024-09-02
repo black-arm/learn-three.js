398b46fc2d0ec36fbcc272e20c429444c12aa271
Antonio Basile <a.basile@innovationengineering.eu>
2024-09-02 13:19:14 +0200

    feat: second lesson

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,27 @@ import * as THREE from 'three'
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: '#00ff00'});
+
+//permette di definire il materiale basic
+//const material = new THREE.MeshBasicMaterial({ color: '#00ff00'});
+
+const material = new THREE.MeshNormalMaterial(); //definisce i colori in maniera casuale
 
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh)
+const mesh2 = mesh.clone();
+const mesh3 = mesh.clone();
+
+scene.add(mesh);
+scene.add(mesh2);
+scene.add(mesh3);
+
+const vector = new THREE.Vector3(2, 2, -5);
+mesh.position.copy(vector);
+mesh.position.add(new THREE.Vector3(1, 0.5, 0));
+mesh3.position.set(-2, 1.5, -3);
+//mesh3.scale.set(0.5, 0.5, 0.5);
+mesh3.scale.multiplyScalar(0.5);
+mesh3.scale.y = 2;
 
 const temp = {
   width: 1024,
@@ -25,6 +42,9 @@ camera.position.z = 3;
 
 function animate(){
   mesh.rotation.y += 0.01;
+  mesh2.rotation.x += 0.03;
+  mesh2.rotation.y += 0.01;
+  mesh3.rotation.y += 0.02
   //mesh.rotation.y += 0.01;
   renderer.render(scene, camera);
   requestAnimationFrame(animate)
